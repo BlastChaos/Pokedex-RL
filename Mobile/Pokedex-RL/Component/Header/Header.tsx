32e43c003b511b0d1c9d5b9fe439adb3b4019619
BlastChaos <76956740+BlastChaos@users.noreply.github.com>
2024-08-25 16:05:58 -0400

    better

---
+++ Mobile/Pokedex-RL/Component/Header/Header.tsx
@@ -0,0 +1,12 @@
+import HeaderLeft from "@/assets/images/header-top-left.svg";
+import HeaderRight from "@/assets/images/header-top-right.svg";
+import React from "react";
+import { View } from "react-native";
+export const Header: React.FC = () => {
+  return (
+    <View className="flex flex-row  h-20">
+      <HeaderLeft />
+      <HeaderRight className="flex flex-1" scaleX={15} />
+    </View>
+  );
+};
