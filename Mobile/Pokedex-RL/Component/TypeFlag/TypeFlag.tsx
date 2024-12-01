5de33f997d500083a6b26231254df98743e61d8b
BlastChaos <76956740+BlastChaos@users.noreply.github.com>
2024-12-01 18:31:43 -0500

    wip

--- Mobile/Pokedex-RL/Component/TypeFlag/TypeFlag.tsx
+++ Mobile/Pokedex-RL/Component/TypeFlag/TypeFlag.tsx
@@ -2,6 +2,7 @@ import { Type } from "@/api/Model/Pokemon";
 import { pokemonTypeColor } from "@/helper/pokemonTypeColor";
 import React from "react";
 import { View, Text } from "react-native";
+import { IconType } from "../IconType/IconType";
 type Props = {
   type: Type;
 };
@@ -11,10 +12,10 @@ export const TypeFlag: React.FC<Props> = (props) => {
       style={{
         backgroundColor: pokemonTypeColor[props.type],
       }}
-      className="rounded-lg w-24 pl-2 flex flex-row items-center"
+      className="rounded-lg w-24 pl-2 h-6 flex flex-row items-center space-x-2"
     >
+      <IconType type={props.type} />
       <Text>{Type[props.type]}</Text>
-      
     </View>
   );
 };
