a0f541e4eeddf7e07580a8ea040699828ece8f23
BlastChaos <76956740+BlastChaos@users.noreply.github.com>
2024-12-23 18:42:44 -0500

    camera work

--- Mobile/Pokedex-RL/app/(tabs)/camera.tsx
+++ Mobile/Pokedex-RL/app/(tabs)/camera.tsx
@@ -1,8 +1,8 @@
-import { View, ImageBackground } from "react-native";
+import { View, ImageBackground, Text } from "react-native";
 
 import { ThemedText } from "@/Component/ThemedText";
 import { CameraView, useCameraPermissions } from "expo-camera";
-import { SafeAreaView } from "react-native-safe-area-context";
+import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
 import { ExpoImage } from "@/Component/ExpoImage/ExpoImage";
 
 export default function Camera() {
@@ -22,21 +22,37 @@ export default function Camera() {
   }
 
   return (
-    <ImageBackground
-      source={require("@/assets/images/camera-bg.jpg")}
-      resizeMode="cover"
-      className="flex flex-col flex-1 items-center"
-    >
-      <SafeAreaView>
-        <View className="flex items-center justify-center">
-          <CameraView>
-            <ExpoImage
-              source={require("@/assets/images/circle-padding-shadow-no-bg.png")}
-              className="w-108 h-108 mt-32"
-            />
-          </CameraView>
-        </View>
+    <SafeAreaProvider>
+      <SafeAreaView className="flex-1">
+        <ImageBackground
+          source={require("@/assets/images/camera-bg.jpg")}
+          resizeMode="cover"
+          className="flex flex-col flex-1 justify-center items-center pb-14 gap-y-6"
+        >
+          <View>
+            <CameraView
+              className="flex justify-center items-center w-108 h-108"
+              style={{
+                borderRadius: 1000,
+              }}
+            >
+              <ExpoImage
+                source={require("@/assets/images/circle-padding-shadow-no-bg.png")}
+                className="w-108 h-108"
+              />
+            </CameraView>
+          </View>
+
+          <View className="gap-2">
+            <Text className="text-white text-4xl italic text-center">
+              {"Camera"}
+            </Text>
+            <Text className="text-gray-500 text-2xl italic text-center">
+              {"Capture a photo of anything"}
+            </Text>
+          </View>
+        </ImageBackground>
       </SafeAreaView>
-    </ImageBackground>
+    </SafeAreaProvider>
   );
 }
