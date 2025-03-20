faff5d8980d9763c6b7dfdd0a496423a59131b09
BlastChaos <76956740+BlastChaos@users.noreply.github.com>
2025-03-19 21:35:44 -0400

    it work. I'm not touching it

--- PokedexRL/apps/mobile/metro.config.js
+++ PokedexRL/apps/mobile/metro.config.js
@@ -1,7 +1,10 @@
 const { getDefaultConfig } = require("expo/metro-config");
 const { withNativeWind } = require("nativewind/metro");
 
-module.exports = (() => {
+const { FileStore } = require("metro-cache");
+const path = require("node:path");
+
+const defaultConfig = (() => {
   const config = getDefaultConfig(__dirname);
 
   const { transformer, resolver } = config;
@@ -16,5 +19,26 @@ module.exports = (() => {
     sourceExts: [...resolver.sourceExts, "svg"],
   };
 
-  return withNativeWind(config, { input: "./global.css" });
+  config.resolver.unstable_enablePackageExports = true;
+
+  return withNativeWind(config, {
+    input: "./global.css",
+  });
 })();
+
+module.exports = withTurborepoManagedCache(defaultConfig);
+
+/**
+ * Move the Metro cache to the `.cache/metro` folder.
+ * If you have any environment variables, you can configure Turborepo to invalidate it when needed.
+ *
+ * @see https://turbo.build/repo/docs/reference/configuration#env
+ * @param {import('expo/metro-config').MetroConfig} config
+ * @returns {import('expo/metro-config').MetroConfig}
+ */
+function withTurborepoManagedCache(config) {
+  config.cacheStores = [
+    new FileStore({ root: path.join(__dirname, ".cache/metro") }),
+  ];
+  return config;
+}
